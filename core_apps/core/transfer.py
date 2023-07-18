239bd198d4a97b7700ac9bab435f169c5beb0adb
John Diginee <Johndbizz@gmail.com>
2023-07-18 16:56:07 +0100

    Feat: Add process & verify amount to be transfered

--- core_apps/core/transfer.py
+++ core_apps/core/transfer.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib import messages
 
+from core_apps.core.models import Transaction
+
 @login_required
 def search_users_account_number(request):
     account = Account.objects.all()
@@ -30,3 +32,44 @@ def AmountTransfer(request, account_number):
         "account": account,
     }
     return render(request, "transfer/amount-transfer.html", context)
+
+def AmountTransferProcess(request, account_number):
+    account = Account.objects.get(account_number=account_number) ## get the sender's account
+    sender = request.user # get the person that is logged in
+    reciever = account.user # get the reciever account
+
+    sender_account = request.user.account # get the currently logged in users account
+    receiver_account = account # get the recievers account 
+
+    if request.method == "POST":
+        amount = request.POST.get("amount-send")
+        description = request.POST.get("description")
+
+        print(amount)
+        print(description)
+
+        if sender_account.account_balance > 0 and amount:
+            """Check avaliable funds, and create new transaction."""
+            new_transaction = Transaction.objects.create(
+                user=request.user,
+                amount=amount,
+                description=description,
+                receiver=reciever,
+                sender=sender,
+                sender_account=sender_account,
+                receiver_account=receiver_account,
+                status="processing",
+                transaction_type="transfer"
+            )
+            new_transaction.save() # save transaction
+            
+            # get the id of the recent transaction
+            transaction_id = new_transaction.transaction_id
+
+            return redirect("core_apps.core:transfer-confirmation", account.account_number, transaction_id)
+        else:
+            messages.warning(request, "Insufficent fund.")
+            return redirect("core_apps.core:amount-transfer", account.account_number)
+    else:
+        messages.warning(request, "Error Occured, Try again.")
+        return redirect("core_apps.account:account")
