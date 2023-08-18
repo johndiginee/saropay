5a9aea5b95a8fc9dba15a923ed7a8c9b36049ad2
John Diginee <Johndbizz@gmail.com>
2023-08-18 19:57:01 +0100

    Chore: Fix bug in User model

--- core_apps/userauths/models.py
+++ core_apps/userauths/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
